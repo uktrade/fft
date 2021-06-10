d442b8e0b13cf54fc67b4645d8f6eda29848788e
Luisella Strona <36708790+Luisella21@users.noreply.github.com>
2021-06-10 13:51:33 +0100

    LST-1395 Fix error with programme field in data lake output

--- data_lake/test/test_programme_code.py
+++ data_lake/test/test_programme_code.py
@@ -5,6 +5,8 @@ from chartofaccountDIT.test.factories import (
     ProgrammeCodeFactory,
 )
 
+from chartofaccountDIT.models import ProgrammeCode
+
 
 class ProgrammeCodeTests(DataLakeTesting):
     def test_data_returned_in_response(self):
@@ -14,6 +16,21 @@ class ProgrammeCodeTests(DataLakeTesting):
             financial_year_id=2019
         ).programme_code
 
+        self.url_name = "data_lake_programme_code"
+
+        self.row_lenght = 4
+        self.code_position = 0
+        self.check_data()
+
+    def test_programme_no_budget_code(self):
+        self.current_code = "643210"
+        obj = ProgrammeCode.objects.create(programme_code=self.current_code)
+        obj.active = True
+        obj.save()
+
+        self.archived_code = HistoricalProgrammeCodeFactory.create(
+            financial_year_id=2019
+        ).programme_code
         self.url_name = "data_lake_programme_code"
 
         self.row_lenght = 4
