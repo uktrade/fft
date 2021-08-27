4ac66315b99dc473879d7f3dafdf5b9bdab8dcb1
Luisella Strona <36708790+Luisella21@users.noreply.github.com>
2021-08-27 11:34:16 +0100

    LST 1436: Added command to clear cache.

---
+++ core/test/test_clear_cache_command.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.core.cache import cache
+from django.core.management import call_command
+
+
+class ClearCacheCommand(TestCase):
+
+    def test_cache(self):
+        original_value = "12345678901234567890"
+        cache_key = "long_cache_key"
+        cache_invalidation_time = 7 * 24 * 60 * 60
+        cache.set(cache_key, original_value, cache_invalidation_time)
+        cached_value = cache.get(cache_key)
+        assert cached_value == original_value
+
+        call_command("clearcache")
+        cached_value = cache.get(cache_key)
+        assert cached_value is None
