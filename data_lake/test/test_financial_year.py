47210a9cace3ad297cfd9a382ad3abfd336c63c3
Luisella Strona <luisella21@hotmail.com>
2021-12-02 09:42:48 +0000

    Added export financila year.

---
+++ data_lake/test/test_financial_year.py
@@ -0,0 +1,16 @@
+from data_lake.test.utils import DataLakeTesting
+from core.models import (
+    FinancialYear,
+)
+
+
+class Analysis1CodeTests(DataLakeTesting):
+    def test_data_returned_in_response(self):
+        financial_year_queryset = FinancialYear.objects.all().order_by("financial_year")
+        self.current_code = financial_year_queryset[0].financial_year
+        self.archived_code = financial_year_queryset[1].financial_year
+
+        self.url_name = "data_lake_financialyear"
+        self.row_lenght = 4
+        self.code_position = 0
+        self.check_data()
