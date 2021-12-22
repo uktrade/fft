d32f028cbf0a070fbaa83e81f57b10ea9c71ef17
Luisella Strona <luisella21@hotmail.com>
2021-12-22 17:41:20 +0000

    Added command for uploading forecast.

--- future_years/management/commands/upload_future_year_forecast.py
+++ future_years/management/commands/upload_future_year_forecast.py
@@ -3,8 +3,9 @@ import os
 from core.utils.command_helpers import (
     CommandUpload,
 )
+from core.utils.generic_helpers import get_current_financial_year
 
-from forecast.import_actuals import upload_trial_balance_report
+from forecast.import_budgets import upload_figure_from_file
 
 from upload_file.models import FileUpload
 
@@ -19,7 +20,14 @@ class Command(CommandUpload):
     def handle(self, *args, **options):
         path = options["path"]
         year = options["financial_year"]
-        file_name = self.path_to_upload(path, 'xslx')
+
+        # Validate the year. It must be in the future
+        current_year = get_current_financial_year()
+        if year <= current_year:
+            self.stdout.write(self.style.ERROR("The year must be in the future."))
+            return
+
+        file_name = self.path_to_upload(path, "xslx")
 
         fileobj = FileUpload(
             document_file_name=file_name,
@@ -27,7 +35,7 @@ class Command(CommandUpload):
             file_location=FileUpload.LOCALFILE,
         )
         fileobj.save()
-        upload_trial_balance_report(fileobj, year)
+        upload_figure_from_file(fileobj, year)
         if self.upload_s3:
             os.remove(file_name)
 
