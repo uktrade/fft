98ee81a4cc25056739b74cc1a293d8483ddb822b
Luisella Strona <luisella21@hotmail.com>
2021-12-30 17:08:04 +0000

    Safety commit

--- end_of_month/test/test_utils.py
+++ end_of_month/test/test_utils.py
@@ -125,6 +125,7 @@ class MonthlyFigureSetup:
             self.monthly_figure_create(period, amount, "Budget")
 
 
+
 class SetFullYearArchive(MonthlyFigureSetup):
     archived_forecast = []
     archived_budget = []
