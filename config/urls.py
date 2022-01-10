71b1793a97d0ea4a19b05476b7274c1ab0c6b3d4
Luisella Strona <luisella21@hotmail.com>
2022-01-10 14:08:52 +0000

    fixed broken teasts.

--- config/urls.py
+++ config/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
     path("chartofaccountDIT/", include("chartofaccountDIT.urls")),
     path("end_of_month/", include("end_of_month.urls")),
     path("forecast/", include("forecast.urls")),
-    path("future_years/", include("future_years.urls")),
     path("gifthospitality/", include("gifthospitality.urls")),
     path("download_file/", include("download_file.urls")),
     path("pingdom/", include("pingdom.urls")),
