eb9e17e607755a18f7387a7749ccea12830872ea
Luisella Strona <luisella21@hotmail.com>
2022-01-13 13:55:51 +0000

    Added test.

---
+++ future_years/test/test_change_year_with_future_data.py
@@ -0,0 +1,16 @@
+from forecast.test.test_new_financial_year_command import  NewFinancialYearTest
+
+
+class NewFinancialYearWithFutureYearDataTest(NewFinancialYearTest):
+    def setUp(self):
+        super().setUp()
+        self.init_data.set_year(self.current_year+1)
+        self.init_data.setup_forecast()
+        self.init_data.setup_budget()
+
+        self.init_data.set_year(self.current_year+2)
+        self.init_data.setup_forecast()
+        self.init_data.setup_budget()
+        self.init_data.set_year(self.current_year)
+
+
