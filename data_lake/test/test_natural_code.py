309bf7ceb95ac4ffe832901d4d0f7b03ccfce0a2
Luisella Strona <36708790+Luisella21@users.noreply.github.com>
2022-01-19 13:29:37 +0000

    LST 1510 Failure in NAC pipeline
    Fixed bug in NAC pipeline for data workspace and created test.

--- data_lake/test/test_natural_code.py
+++ data_lake/test/test_natural_code.py
@@ -23,3 +23,22 @@ class NaturalCodeTests(DataLakeTesting):
         self.row_lenght = 10
         self.code_position = 7
         self.check_data()
+
+
+class ArchivedNaturalCodeTests(DataLakeTesting):
+    def test_data_returned_in_response(self):
+        self.current_code = "12345678"
+        past_year = 2019
+        NaturalCodeFactory.create(natural_account_code=self.current_code)
+        self.archived_code = HistoricalNaturalCodeFactory.create(
+            financial_year_id=past_year,
+            expenditure_category=HistoricalExpenditureCategoryFactory.create(
+                financial_year_id=past_year,
+                linked_budget_code=12345,
+            ),
+        ).natural_account_code
+
+        self.url_name = "data_lake_natural_code"
+        self.row_lenght = 10
+        self.code_position = 7
+        self.check_data()
