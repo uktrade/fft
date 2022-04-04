3094132fd65ad039b2210a11aab7bede226733ef
Luisella Strona <luisella21@hotmail.com>
2022-04-04 17:47:08 +0100

    Added class for data report.

--- mi_report_data/models.py
+++ mi_report_data/models.py
@@ -8,19 +8,22 @@ from forecast.models import FinancialCode, FinancialPeriod
 # financial_period_id, financial_year_id, archived_period_id
 
 
-class ForecastingDataView(models.Model):
+class ReportDataView(models.Model):
     id = models.IntegerField(primary_key=True,)
-    financial_code = models.ForeignKey(FinancialCode, on_delete=models.DO_NOTHING,)
+    financial_code = models.ForeignKey(FinancialCode,
+                                       on_delete=models.DO_NOTHING,
+                                       related_name="mi_report_data_financial_code"
+                                       )
     financial_year_id = models.IntegerField()
     financial_period = models.ForeignKey(
         FinancialPeriod,
-        on_delete=models.PROTECT,
-        related_name="%(app_label)s_%(class)ss",
+        on_delete=models.DO_NOTHING,
+        related_name="mi_report_data_financial_period",
     )
     archived_period_id = models.ForeignKey(
         FinancialPeriod,
-        on_delete=models.PROTECT,
-        related_name="archived_period",
+        on_delete=models.DO_NOTHING,
+        related_name="mi_report_data_archived_period",
     )
     budget = models.BigIntegerField(default=0)
     forecast = models.BigIntegerField(default=0)
