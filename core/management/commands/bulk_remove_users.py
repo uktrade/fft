f8ab671c4ecfb664089aff9be5bcb978a9aa4436
Luisella Strona <luisella21@hotmail.com>
2022-04-13 15:14:57 +0100

    Added command to delete users.

---
+++ core/management/commands/bulk_remove_users.py
@@ -0,0 +1,35 @@
+import os
+
+from core.utils.command_helpers import (
+    CommandUpload,
+)
+
+from core.remove_users import bulk_delete_users
+
+from upload_file.models import FileUpload
+
+
+class Command(CommandUpload):
+    help = "Upload the Trial Balance for a specific month"
+
+    def add_arguments(self, parser):
+        parser.add_argument("path")
+
+    def handle(self, *args, **options):
+        path = options["path"]
+        file_name = self.path_to_upload(path, 'xslx')
+
+        fileobj = FileUpload(
+            document_file_name=file_name,
+            document_type=FileUpload.OTHER,
+            file_location=FileUpload.LOCALFILE,
+        )
+        fileobj.save()
+        howmany = bulk_delete_users(fileobj)
+        if self.upload_s3:
+            os.remove(file_name)
+
+        self.stdout.write(
+            self.style.SUCCESS( f"Deleted {howmany} users.")
+            )
+
