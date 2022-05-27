cc6f04c23904d2f42620fd258cfd51d50ed8c06d
Luisella Strona <36708790+Luisella21@users.noreply.github.com>
2022-05-27 09:25:31 +0100

    LST 1606 edit forecast introduces extra rows (#372)
    Create command to delete unused financial codes.
    The financial codes were not deleted at the end of the financial year, because of a bug in the deletion.
    Running the new command fix the error found by the user.

---
+++ forecast/test/test_clear_financial_codes.py
@@ -0,0 +1,25 @@
+from io import StringIO
+
+from django.test import TestCase
+from django.core.management import call_command
+
+from end_of_month.test.test_utils import MonthlyFigureSetup
+
+from forecast.models import (
+    FinancialCode,
+)
+from forecast.test.factories import FinancialCodeFactory
+
+
+class ClearForecastCommandNoArchiveTest(TestCase):
+    def setUp(self):
+        self.out = StringIO()
+        init_data = MonthlyFigureSetup()
+        init_data.setup_forecast()
+        init_data.setup_budget()
+        FinancialCodeFactory()
+
+    def test_dont_remove_in_use(self):
+        assert FinancialCode.objects.all().count() == 2
+        call_command("clear_financial_codes")
+        assert FinancialCode.objects.all().count() == 1
