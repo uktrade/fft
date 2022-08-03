53e13d31252d1ab995cc27226e29ca16802af8ac
Luisella Strona <luisella21@hotmail.com>
2022-08-03 09:28:19 +0100

    Safety commit.

--- mi_report_data/models.py
+++ mi_report_data/models.py
@@ -6,34 +6,76 @@ from forecast.models import FinancialCode, FinancialPeriod
 
 # financial_code_id, budget, forecast, actual,
 # financial_period_id, financial_year_id, archived_period_id
-
-
-class ReportDataView(models.Model):
-    id = models.IntegerField(primary_key=True,)
-    financial_code = models.ForeignKey(FinancialCode,
-                                       on_delete=models.DO_NOTHING,
-                                       related_name="mi_report_data_financial_code"
-                                       )
+class UniqueDataKey(models.Model):
+    financial_code = models.ForeignKey(
+        FinancialCode,
+        on_delete=models.DO_NOTHING,
+        related_name="financial_code_%(app_label)s_%(class)ss",
+    )
     financial_year_id = models.IntegerField()
     financial_period = models.ForeignKey(
         FinancialPeriod,
         on_delete=models.DO_NOTHING,
-        related_name="mi_report_data_financial_period",
+        related_name="financial_period_%(app_label)s_%(class)ss",
     )
     archived_period = models.ForeignKey(
         FinancialPeriod,
         on_delete=models.DO_NOTHING,
-        related_name="mi_report_data_archived_period",
+        related_name="financial_archived_period_%(app_label)s_%(class)ss",
     )
+
+    class Meta:
+        abstract = True
+
+
+class ReportDataView(UniqueDataKey):
+    id = models.IntegerField(primary_key=True,)
     budget = models.BigIntegerField(default=0)
     forecast = models.BigIntegerField(default=0)
     actual = models.BigIntegerField(default=0)
 
     class Meta:
         managed = False
-        # db_table = "mi_report_data_query"
         db_table = "mi_report_full_data"
         default_permissions = "view"
         permissions = [
             ("can_view_mi_report_data", "Can view MI report data"),
         ]
+
+
+class ReportPreviousMonthDataView(UniqueDataKey):
+    id = models.IntegerField(primary_key=True,)
+    previous_month_forecast = models.BigIntegerField(default=0)
+    class Meta:
+        managed = False
+        db_table = "mi_report_full_data"
+        default_permissions = "view"
+
+
+# class ReportPreviousYearDataView(UniqueDataKey):
+#     id = models.IntegerField(primary_key=True,)
+#     previous_year_actual = models.BigIntegerField(default=0)
+#     class Meta:
+#         managed = False
+#         db_table = "mi_report_full_data"
+#         default_permissions = "view"
+#
+#
+# # The following data are calculated using SQL.
+# # They will become derived tables in Dataworkspace
+# class ReportYTDView(UniqueDataKey):
+#     ytd_budget = models.BigIntegerField(default=0)
+#     ytd_actual = models.BigIntegerField(default=0)
+#     class Meta:
+#         managed = False
+#         db_table = "mi_report_full_data"
+#         default_permissions = "view"
+#
+#
+# class ReportFullYearView(UniqueDataKey):
+#     full_year_budget = models.BigIntegerField(default=0)
+#     full_year_total = models.BigIntegerField(default=0)
+#     class Meta:
+#         managed = False
+#         db_table = "mi_report_full_data"
+#         default_permissions = "view"
