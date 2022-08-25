c8e0b7d8b3f3d1d48fab66957cbe7146e9555bfa
Luisella Strona <luisella21@hotmail.com>
2022-08-25 13:57:02 +0100

    Removed print statements, and fixed queries.

--- mi_report_data/views.py
+++ mi_report_data/views.py
@@ -101,10 +101,6 @@ class MIReportFieldList(FigureFieldData):
                 "archiving_year",
             )
         )
-        print("=================")
-        print(forecast_queryset.query)
-        print("=================")
-
         for row in forecast_queryset:
             writer.writerow(row)
 
