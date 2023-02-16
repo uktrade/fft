858785f4c8700ed7d442aa0833d913b970f4a1db
Luisella Strona <luisella21@hotmail.com>
2023-02-16 15:18:04 +0000

    Added budget tests.

--- data_lake/test/test_mi_report/test_past_year_actual.py
+++ data_lake/test/test_mi_report/test_past_year_actual.py
@@ -1,13 +1,10 @@
-from core.utils.generic_helpers import get_current_financial_year
 from data_lake.test.utils import DataLakeTesting
-from end_of_month.test.test_utils import SetFullYearArchive
 from previous_years.test.test_utils import PastYearForecastSetup
 
 
 class PastActualTests(DataLakeTesting, PastYearForecastSetup):
     def test_data_returned_in_response(self):
 
-        SetFullYearArchive(year=get_current_financial_year() - 1)
         self.url_name = "mi_report_previous_year_data"
         response = self.get_data()
 
