1a9e6f9c47e992e803911808e44ed250710b9112
Luisella Strona <luisella21@hotmail.com>
2023-02-16 14:46:05 +0000

    Black and isort

--- data_lake/test/test_mi_report/test_future_year_forecast.py
+++ data_lake/test/test_mi_report/test_future_year_forecast.py
@@ -1,7 +1,4 @@
-from core.utils.generic_helpers import (
-    get_current_financial_year,
-)
-
+from core.utils.generic_helpers import get_current_financial_year
 from data_lake.test.utils import DataLakeTesting
 from end_of_month.test.test_utils import SetFullYearArchive
 
@@ -9,7 +6,7 @@ from end_of_month.test.test_utils import SetFullYearArchive
 class FutureForecastTests(DataLakeTesting):
     def test_data_returned_in_response(self):
 
-        SetFullYearArchive(year=get_current_financial_year()+1)
+        SetFullYearArchive(year=get_current_financial_year() + 1)
         self.url_name = "mi_report_future_year_forecast_data"
         response = self.get_data()
 
