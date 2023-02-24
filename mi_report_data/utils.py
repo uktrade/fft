563fc11f95d4f4117109c8065498ebe50373ea36
Luisella Strona <luisella21@hotmail.com>
2023-02-24 15:46:47 +0000

    Added a TODO

--- mi_report_data/utils.py
+++ mi_report_data/utils.py
@@ -1,6 +1,6 @@
 from django.db import connection
 
-
+# TODO remove materialised view mi_report_periods. It does not seem to be used anywhere
 def refresh_materialised_views():
     # Brute force: execute an sql query refreshing all the materialized views
     # used to get the archived data in the correct format for the data workspace
