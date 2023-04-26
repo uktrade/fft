873db660bbd3b3096a5e15ee726b96387c6fcd59
Luisella Strona <luisella21@hotmail.com>
2023-04-26 18:06:37 +0100

    Command to clear the split data tables.

---
+++ upload_split_file/management/commands/clear_split_data.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from upload_split_file.models import PaySplitCoefficient, UploadPaySplitCoefficient, SplitPayActualFigure, TemporaryCalculatedValues
+
+
+class Command(BaseCommand):
+    help = "Delete split data."
+
+    def handle(self, *args, **options):
+        # Clear the split data. It is not in use yet,
+        # but it could break the archive
+        UploadPaySplitCoefficient.objects.all().delete()
+        PaySplitCoefficient.objects.all().delete()
+        SplitPayActualFigure.objects.all().delete()
+        SplitPayActualFigure.objects.all().delete()
+        TemporaryCalculatedValues.objects.all().delete()
+
+        self.stdout.write(self.style.SUCCESS("Split data table cleared. "))
