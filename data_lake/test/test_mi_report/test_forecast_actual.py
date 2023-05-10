d47ab38707ffe8edd0a15f62397a6957a6cbf5d4
Luisella Strona <luisella21@hotmail.com>
2023-05-10 14:53:38 +0100

    Fixed test.

--- data_lake/test/test_mi_report/test_forecast_actual.py
+++ data_lake/test/test_mi_report/test_forecast_actual.py
@@ -13,4 +13,18 @@ class Forecast1Tests(DataLakeTesting):
 
         cols = rows[0].split(",")
         assert len(cols) == 17
-        assert len(rows) == 170
+        assert len(rows) == 74
+
+class Forecast2Tests(DataLakeTesting):
+    def test_forecast_data_returned_in_response(self):
+        SetFullYearArchive()
+        self.url_name = "mi_report_forecast_data_2"
+        response = self.get_data()
+
+        assert response["Content-Type"] == "text/csv"
+        rows = response.content.decode("utf-8").split("\n")
+
+        cols = rows[0].split(",")
+        assert len(cols) == 17
+        assert len(rows) == 98
+
