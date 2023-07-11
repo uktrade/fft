eb40c67a91ee132fcc3eff76a77071f8193ce09f
Yusuf <yusuf@madetech.com>
2023-07-11 13:28:22 +0100

    Apply `isort` changes

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 import os
+
 import dj_database_url
 import environ
-from dbt_copilot_python.database import database_from_env, database_url_from_env
+from dbt_copilot_python.database import database_url_from_env
 from dbt_copilot_python.network import setup_allowed_hosts
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
@@ -100,15 +101,6 @@ if env("ELASTIC_APM_ENVIRONMENT", default=None):
         "SERVER_URL": "https://apm.elk.uktrade.digital",
         "ENVIRONMENT": env("ELASTIC_APM_ENVIRONMENT", default=None),
     }
-
-# DATABASE_CREDENTIALS = env.json("DATABASE_CREDENTIALS", default={})
-#
-# os.environ[
-#     "DATABASE_URL"
-# ] = "{engine}://{username}:{password}@{host}:{port}/{dbname}".format(
-#     **DATABASE_CREDENTIALS
-# )
-# DATABASES = database_from_env("DATABASE_CREDENTIALS")
 
 DATABASES = {
     'default': dj_database_url.config(
