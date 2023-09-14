9328059c64fa02ab101caa4dccec4b245c946207
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2023-09-14 11:46:15 +0100

    add newline to settings imports

--- config/settings/build.py
+++ config/settings/build.py
@@ -4,6 +4,7 @@ manage.py compilestatic --settings=config.settings.build
 """
 import os
 
+
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 
 SECRET_KEY = "dont-use-in-prod"
