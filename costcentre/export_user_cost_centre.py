692a442fb4bfda1a5028344367ca840d01e82cb9
Luisella Strona <luisella21@hotmail.com>
2023-09-15 09:50:14 +0100

    Changed to export to Excel

--- costcentre/export_user_cost_centre.py
+++ costcentre/export_user_cost_centre.py
@@ -1,22 +1,25 @@
-import csv
+import openpyxl
 
 from django.http import HttpResponse
 from guardian.shortcuts import get_users_with_perms
 from core.utils.generic_helpers import today_string
+from core.utils.export_helpers import EXC_TAB_NAME_LEN, EXCEL_TYPE
 
 from costcentre.models import CostCentre
 
 
 def export_cost_centres(request):
     title = f"cost_centre_users_{today_string()}"
-    response = HttpResponse(content_type="text/csv")
-    response["Content-Disposition"] = "attachment; filename=" + title + ".csv"
-    writer = csv.writer(response, csv.excel)
-    response.write("\ufeff".encode("utf8"))  # Excel needs UTF-8 to open the file
-    writer.writerow(["Cost Centre", "User last name","User first name", ])
+    response = HttpResponse(content_type=EXCEL_TYPE)
+    response["Content-Disposition"] = "attachment; filename=" + title + ".xlsx"
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.title = title[:EXC_TAB_NAME_LEN]
+    ws.append(["Cost Centre", "User last name","User first name", ])
     cost_centre_queryset = CostCentre.objects.filter(active=True)
     for cost_centre in cost_centre_queryset:
         users = get_users_with_perms(cost_centre, attach_perms=True)
         for user in users:
-            writer.writerow([cost_centre.cost_centre_code, user.last_name, user.first_name])
+            ws.append([cost_centre.cost_centre_code, user.last_name, user.first_name])
+    wb.save(response)
     return response
