ed434197d7f5a049de59b0274a387fb46bb8c150
Gabriel Naughton <gabriel.naughton@digital.trade.gov.uk>
2023-09-15 14:03:46 +0100

    add npm run build to user-post

--- user-post.sh
+++ user-post.sh
@@ -3,6 +3,7 @@
 echo "Running post build script"
 pip install -r requirements.txt
 npm ci
+npm run build
 
 python manage.py compilescss --settings=config.settings.build
 python manage.py collectstatic --settings=config.settings.build --noinput
