783977bd9fb2bc329aa66763c495fdcad4ed3a49
Luisella Strona <36708790+Luisella21@users.noreply.github.com>
2023-09-19 17:06:47 +0100

    Download cost centre user (#454)
    New button in the Admin interface to download the users and the cost centre relationship.
    Several changes to the circleci config file, because it stopped working.

---
+++ user/download_user_cost_centre.py
@@ -0,0 +1,30 @@
+from guardian.shortcuts import get_users_with_perms
+
+from core.utils.export_helpers import export_to_excel
+from core.utils.generic_helpers import today_string
+from costcentre.models import CostCentre
+
+
+def download_cost_centre_user_iterator(queryset):
+    yield [
+        "Cost Centre",
+        "First Name",  # /PS-IGNORE
+        "Last Name",  # /PS-IGNORE
+    ]
+    for cost_centre in queryset:
+        users = get_users_with_perms(cost_centre, attach_perms=True)
+        for user in users:
+            yield [
+                cost_centre.cost_centre_code,
+                user.first_name,
+                user.last_name,
+            ]
+
+
+def download_cost_centres(request):
+    title = f"cost_centre_users_{today_string()}"
+    return export_to_excel(
+        CostCentre.objects.filter(active=True),
+        download_cost_centre_user_iterator,
+        title,
+    )
