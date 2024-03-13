25bb31ea3d5c8f22e933c920a972d64fdbf2fd1c
Cameron Lamb <cameron.lamb@digital.trade.gov.uk>
2024-03-13 17:11:26 +0000

    Update django-compressor

--- config/settings/base.py
+++ config/settings/base.py
@@ -323,10 +323,8 @@ CACHES = {
 from urllib.parse import quote
 
 import django
-from django.utils.encoding import force_str, smart_str
+from django.utils.encoding import smart_str
 
-# Need to FORK compressor to fix this
-django.utils.encoding.force_text = force_str
 # Need to FORK django-bootstrap-breadcrumbs to fix this
 django.utils.encoding.smart_text = smart_str
 # Need to FORK django-adv-cache-tag to fix this
