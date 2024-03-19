19ab0e9126b3d20754a76a6bcade59d2c11765c2
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-03-19 14:59:16 +0000

    wip

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,12 @@ export default defineConfig({
     outDir: "build",
     manifest: true,
     rollupOptions: {
-      input: [
-        "front_end/src/index.jsx",
-        "front_end/styles/styles.scss",
-      ],
+      input: ["front_end/src/index.jsx", "front_end/styles/styles.scss"],
+    },
+  },
+  server: {
+    proxy: {
+      "/static/": "http://localhost:8000",
     },
   },
 });
