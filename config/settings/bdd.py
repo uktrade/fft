fc8e7bc683945885025665b72590a48fbd6e4763
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-03-20 09:39:19 +0000

    wip

--- config/settings/bdd.py
+++ config/settings/bdd.py
@@ -8,10 +8,11 @@ CAN_ELEVATE_SSO_USER_PERMISSIONS = True
 INSTALLED_APPS += ("behave_django",)
 INSTALLED_APPS.remove("authbroker_client")
 
-# The bdd tests are run with behave-django which uses StaticLiveServerTestCase. WhiteNoise
+# The bdd tests are run with behave-django which uses a StaticLiveServerTestCase behind
+# the scenes. WhiteNoise does not work with StaticLiveServerTestCase so we need switch
+# back to the default StaticFilesStorage.
 STORAGES["staticfiles"] = {
-    # "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-    "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
 }
 
 SELENIUM_HOST = env("SELENIUM_HOST", default="web")
