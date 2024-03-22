23b88025b6e39c096021ac89eadaaa7bd1792578
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-03-22 09:28:13 +0000

    samwip

--- core/management/commands/create_stub_data.py
+++ core/management/commands/create_stub_data.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
 
-import core
 from chartofaccountDIT.models import (
     Analysis1,
     Analysis2,
--- gifthospitality/management/commands/populate_gift_hospitality_table.py
+++ gifthospitality/management/commands/populate_gift_hospitality_table.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
 
-import core
 from gifthospitality.models import (
     GiftAndHospitalityCategory,
     GiftAndHospitalityClassification,
