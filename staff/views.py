eecbec0146e85768717a0726e4e8eb9c9303f645
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-10-08 16:59:18 +0100

    wip

--- staff/views.py
+++ staff/views.py
@@ -17,7 +17,7 @@ def superuser_view(view_func):
     def wrapper(request, *args, **kwargs):
         if not request.user.is_superuser:
             raise PermissionDenied
-        return view_func(*args, **kwargs)
+        return view_func(request, *args, **kwargs)
 
     return wrapper
 
