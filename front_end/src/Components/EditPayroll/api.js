238712ec320f6f434da3c5568202fa275a62e5ff
Caitlin Barnard <caitlin@madetech.com>
2024-10-17 15:43:33 +0100

    Spelling fix

--- front_end/src/Components/EditPayroll/api.js
+++ front_end/src/Components/EditPayroll/api.js
@@ -14,7 +14,7 @@ export function getPayrollData() {
  * Post modified payroll data.
  *
  * @param {types.PayrollData[]} payrollData - Payroll data to be sent.
- * @returns {import("../../Util").PostDataResponse} - Updated payroll data receieved.
+ * @returns {import("../../Util").PostDataResponse} - Updated payroll data received.
  */
 export function postPayrollData(payrollData) {
   return postData(getPayrollApiUrl(), payrollData);
