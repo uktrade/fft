f7561ed23e152490a85b7516d76f200c4182e06c
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-10-21 11:13:19 +0100

    Working on payroll API (#535)

--- payroll/services/payroll.py
+++ payroll/services/payroll.py
@@ -1,6 +1,8 @@
 from decimal import Decimal
+from typing import Iterator, TypedDict
 
 from django.db.models import F, Q, Sum
+from django.db import transaction
 
 from core.models import FinancialYear
 from costcentre.models import CostCentre
@@ -48,3 +50,51 @@ def payroll_forecast_report(cost_centre: CostCentre) -> None:
     )
 
     return qs
+
+
+class EmployeePayroll(TypedDict):
+    name: str
+    employee_no: str
+    pay_periods: list[bool]
+
+
+def get_payroll_data(
+    cost_centre: CostCentre,
+    financial_year: FinancialYear,
+) -> Iterator[EmployeePayroll]:
+    qs = EmployeePayPeriods.objects.select_related("employee")
+    qs = qs.filter(
+        employee__cost_centre=cost_centre,
+        year=financial_year,
+    )
+    for obj in qs:
+        yield EmployeePayroll(
+            name=obj.employee.get_full_name(),
+            employee_no=obj.employee.employee_no,
+            pay_periods=obj.periods,
+        )
+
+
+@transaction.atomic
+def update_payroll_data(
+    cost_centre: CostCentre,
+    financial_year: FinancialYear,
+    payroll_data: list[EmployeePayroll],
+) -> None:
+    for payroll in payroll_data:
+        if not payroll["employee_no"]:
+            raise ValueError("employee_no is empty")
+
+        if len(payroll["pay_periods"]) != 12:
+            raise ValueError("pay_periods list should be of length 12")
+
+        if not all(isinstance(x, bool) for x in payroll["pay_periods"]):
+            raise ValueError("pay_periods items should be of type bool")
+
+        pay_periods = EmployeePayPeriods.objects.get(
+            employee__employee_no=payroll["employee_no"],
+            employee__cost_centre=cost_centre,
+            year=financial_year,
+        )
+        pay_periods.periods = payroll["pay_periods"]
+        pay_periods.save()
