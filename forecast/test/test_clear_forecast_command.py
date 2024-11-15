db129a74653bde733abc7e741f8abeb3fccd72af
Caitlin Barnard <caitlin@madetech.com>
2024-11-15 16:39:56 +0000

    Refactor test to remove duplication

--- forecast/test/test_clear_forecast_command.py
+++ forecast/test/test_clear_forecast_command.py
@@ -12,146 +12,62 @@ from forecast.models import BudgetMonthlyFigure, FinancialCode, ForecastMonthlyF
 
 class ClearForecastCommandNoArchiveTest(TestCase):
     def setUp(self):
-        self.out = StringIO()
         init_data = MonthlyFigureSetup()
         init_data.setup_forecast()
         init_data.setup_budget()
         self.current_year = get_current_financial_year()
 
-    def test_no_answer(self):
-        self.assertNotEqual(
-            BudgetMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
+    def assertFiguresCount(self, should_exist=True):
+        count_assert = self.assertNotEqual if should_exist else self.assertEqual
+        count_assert(
+            BudgetMonthlyFigure.objects.filter(financial_year=self.current_year).count(),
             0,
         )
-        self.assertNotEqual(
+        count_assert(
             ForecastMonthlyFigure.objects.filter(
                 financial_year=self.current_year
             ).count(),
             0,
         )
-        self.assertNotEqual(FinancialCode.objects.all().count(), 0)
+        count_assert(FinancialCode.objects.all().count(), 0)
+
+    def test_no_answer(self):
+        self.assertFiguresCount()
         with captured_stdin() as stdin:
             stdin.write("n\n")
             stdin.seek(0)
             with self.assertRaises(CommandError):
                 call_command("clear_forecast")
-        self.assertNotEqual(
-            BudgetMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(
-            ForecastMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(FinancialCode.objects.all().count(), 0)
+        self.assertFiguresCount()
 
     def test_yes_no_answer(self):
-        self.assertNotEqual(
-            BudgetMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(
-            ForecastMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(FinancialCode.objects.all().count(), 0)
+        self.assertFiguresCount()
         with captured_stdin() as stdin:
             stdin.write("y\n")
             stdin.write("n")
             stdin.seek(0)
             with self.assertRaises(CommandError):
                 call_command("clear_forecast")
-        self.assertNotEqual(
-            BudgetMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(
-            ForecastMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(FinancialCode.objects.all().count(), 0)
+        self.assertFiguresCount()
 
     def test_yes_yes_answer(self):
-        self.assertNotEqual(
-            BudgetMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(
-            ForecastMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(FinancialCode.objects.all().count(), 0)
+        self.assertFiguresCount()
         with captured_stdin() as stdin:
             stdin.write("y\n")
             stdin.write("y")
             stdin.seek(0)
             call_command("clear_forecast")
-        self.assertEqual(
-            BudgetMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertEqual(
-            ForecastMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertEqual(FinancialCode.objects.all().count(), 0)
+        self.assertFiguresCount(False)
 
     def test_not_interactive(self):
-        self.assertNotEqual(
-            BudgetMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(
-            ForecastMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(FinancialCode.objects.all().count(), 0)
+        self.assertFiguresCount()
         with self.assertRaises(CommandError):
             call_command("clear_forecast", "--noinput")
-        self.assertNotEqual(
-            BudgetMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(
-            ForecastMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(FinancialCode.objects.all().count(), 0)
+        self.assertFiguresCount()
 
 
 class ClearForecastCommandWithArchiveTest(TestCase):
     def setUp(self):
-        self.out = StringIO()
         init_data = MonthlyFigureSetup()
         init_data.setup_forecast()
         init_data.setup_budget()
@@ -161,96 +77,38 @@ class ClearForecastCommandWithArchiveTest(TestCase):
             "archive_current_year",
         )
 
-    def test_no_answer(self):
-        self.assertNotEqual(
-            BudgetMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
+    def assertFiguresCount(self, should_exist=True):
+        count_assert = self.assertNotEqual if should_exist else self.assertEqual
+        count_assert(
+            BudgetMonthlyFigure.objects.filter(financial_year=self.current_year).count(),
             0,
         )
-        self.assertNotEqual(
+        count_assert(
             ForecastMonthlyFigure.objects.filter(
                 financial_year=self.current_year
             ).count(),
             0,
         )
-        self.assertNotEqual(FinancialCode.objects.all().count(), 0)
+        count_assert(FinancialCode.objects.all().count(), 0)
+
+    def test_no_answer(self):
+        self.assertFiguresCount()
         with captured_stdin() as stdin:
             stdin.write("n\n")
             stdin.seek(0)
             with self.assertRaises(CommandError):
                 call_command("clear_forecast")
-        self.assertNotEqual(
-            BudgetMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(
-            ForecastMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(FinancialCode.objects.all().count(), 0)
+        self.assertFiguresCount()
 
     def test_yes_answer(self):
-        self.assertNotEqual(
-            BudgetMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(
-            ForecastMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(FinancialCode.objects.all().count(), 0)
+        self.assertFiguresCount()
         with captured_stdin() as stdin:
             stdin.write("y")
             stdin.seek(0)
             call_command("clear_forecast")
-        self.assertEqual(
-            BudgetMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertEqual(
-            ForecastMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertEqual(FinancialCode.objects.all().count(), 0)
+        self.assertFiguresCount(False)
 
     def test_not_interactive(self):
-        self.assertNotEqual(
-            BudgetMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(
-            ForecastMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertNotEqual(FinancialCode.objects.all().count(), 0)
+        self.assertFiguresCount()
         call_command("clear_forecast", "--noinput")
-        self.assertEqual(
-            BudgetMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertEqual(
-            ForecastMonthlyFigure.objects.filter(
-                financial_year=self.current_year
-            ).count(),
-            0,
-        )
-        self.assertEqual(FinancialCode.objects.all().count(), 0)
+        self.assertFiguresCount(False)
