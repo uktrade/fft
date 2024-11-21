a4b8e706c4d6efeab7ef5e04d05878a7c4b8b0f6
Caitlin Barnard <caitlin@madetech.com>
2024-11-21 13:48:54 +0000

    Refactor payroll component headers and add vacancies methods

--- front_end/src/Components/EditPayroll/index.jsx
+++ front_end/src/Components/EditPayroll/index.jsx
@@ -7,28 +7,7 @@ import PayrollTable from "./PayrollTable/index";
  * @param {types.PayrollData[]} props.payroll
  * @returns
  */
-export default function EditPayroll({ payroll, onTogglePayPeriods }) {
-  const headers = [
-    "Name",
-    "Grade",
-    "Employee No",
-    "FTE",
-    "Programme Code",
-    "Budget Type",
-    "Assignment Status",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-    "Jan",
-    "Feb",
-    "Mar",
-  ];
+export default function EditPayroll({ payroll, headers, onTogglePayPeriods }) {
   return (
     <PayrollTable
       headers={headers}
