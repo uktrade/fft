a40ccf1f857d51f502febab2cf9f6ee202c79ca9
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-11-25 14:00:29 +0000

    wip

--- payroll/tests/services/test_payroll.py
+++ payroll/tests/services/test_payroll.py
@@ -29,8 +29,14 @@ def test_payroll_forecast(db):
         group__natural_code__natural_account_code=PENSION_NAC,
     )
 
-    employees = EmployeeFactory.create_batch(
-        size=10,
+    payroll_employees = EmployeeFactory.create_batch(
+        size=2,
+        cost_centre__cost_centre_code="123456",
+        programme_code__programme_code="123456",
+        grade__grade="Grade 7",
+    )
+    non_payroll_employees = EmployeeFactory.create_batch(
+        size=2,
         cost_centre__cost_centre_code="123456",
         programme_code__programme_code="123456",
         grade__grade="Grade 7",
@@ -45,5 +51,6 @@ def test_payroll_forecast(db):
         + (1500 - 55.6)  # salary_1 debit - credit
         + (80 - 0)  # + salary_2 debit - credit
         + (130.25 - 15)  # + pension debit - credit
+        # employee 3 and 4 are non-payroll (no basic pay)
     )
     assert expected == 3665.15
