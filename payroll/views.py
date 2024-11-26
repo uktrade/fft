1cb4e00945c49fb1d5ed7c8429a5526b44c86f4c
Caitlin Barnard <caitlin@madetech.com>
2024-11-26 10:31:58 +0000

    Pass url through window object

--- payroll/views.py
+++ payroll/views.py
@@ -5,7 +5,6 @@ from django.core.exceptions import PermissionDenied
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.template.response import TemplateResponse
-from django.urls import reverse
 from django.views import View
 
 from core.models import FinancialYear
@@ -100,16 +99,6 @@ def edit_payroll_page(
     cost_centre_code = cost_centre_obj.cost_centre_code
     financial_year = financial_year_obj.financial_year
 
-    add_vacancy_url = (
-        reverse(
-            "payroll:add_vacancy",
-            kwargs={
-                "cost_centre_code": cost_centre_code,
-                "financial_year": financial_year,
-            },
-        ),
-    )
-
     context = {
         "cost_centre_code": cost_centre_code,
         "financial_year": financial_year,
@@ -128,7 +117,6 @@ def edit_payroll_page(
             "Feb",
             "Mar",
         ],
-        "add_vacancy_url": add_vacancy_url,
     }
 
     return TemplateResponse(request, "payroll/page/edit_payroll.html", context)
