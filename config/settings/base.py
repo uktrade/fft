9d5954ebd4243aebb5b1b94a888f5665278d1fc7
Caitlin Barnard <caitlin@madetech.com>
2024-11-29 11:43:13 +0000

    Readd use TZ

--- config/settings/base.py
+++ config/settings/base.py
@@ -147,6 +147,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = "en-gb"  # must be gb for date entry to work
 TIME_ZONE = "UTC"
 USE_I18N = True
+USE_TZ = True
 
 
 # Remove extra details in the label
