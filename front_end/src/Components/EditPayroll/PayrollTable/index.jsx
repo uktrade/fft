6a15ad9202566f69af042e08d0accc2f5e9bd77e
Caitlin Barnard <54268863+CaitBarnard@users.noreply.github.com>
2024-12-02 16:07:26 +0000

    Fix tables overflowing on Edit Payroll (#555)
    - [ ] JIRA ticket referenced in title
    - [ ] Title is clear and concise
    - [ ] Description gives any relevant detail
    - [ ] Tests are up to date
    - [ ] Documentation is up to date

--- front_end/src/Components/EditPayroll/PayrollTable/index.jsx
+++ front_end/src/Components/EditPayroll/PayrollTable/index.jsx
@@ -12,26 +12,31 @@ export default function PayrollTable({
 }) {
   return (
     <>
-      <table className="govuk-table">
-        <thead className="govuk-table__head">
-          <tr className="govuk-table__row">
-            {headers.map((header) => {
+      <div class="scrollable">
+        <table className="govuk-table">
+          <thead className="govuk-table__head">
+            <tr className="govuk-table__row">
+              {headers.map((header) => {
+                return (
+                  <th scope="col" className="govuk-table__header" key={header}>
+                    {header}
+                  </th>
+                );
+              })}
+            </tr>
+          </thead>
+          <tbody className="govuk-table__body">
+            {payroll.map((row) => {
               return (
-                <th scope="col" className="govuk-table__header" key={header}>
-                  {header}
-                </th>
+                <RowComponent
+                  row={row}
+                  onTogglePayPeriods={onTogglePayPeriods}
+                />
               );
             })}
-          </tr>
-        </thead>
-        <tbody className="govuk-table__body">
-          {payroll.map((row) => {
-            return (
-              <RowComponent row={row} onTogglePayPeriods={onTogglePayPeriods} />
-            );
-          })}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
