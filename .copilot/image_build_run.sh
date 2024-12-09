c64a57378ea47d75dffa443e6fd7fd1c3ada3adc
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-12-09 17:05:46 +0000

    wip

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
 # Exit early if something goes wrong
-set -ex
+set -e
+
+export DJANGO_SETTINGS_MODULE=config.settings.prod
+
+cp .env.ci .env
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
-
-# Set export all variables
-set -o allexport
-# Source .env.ci as the env file for building
-# shellcheck source=/dev/null
-. ./.env.ci
-# Unset export all variables
-set +o allexport
 
 python manage.py collectstatic --noinput
