42d87cf6ddfd160d5e16dee659a143e0e8737581
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-12-09 15:04:38 +0000

    wip

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -4,6 +4,6 @@
 set -ex
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
-export $(grep -v '^#' .env.ci | xargs -d '\n')
+export $(grep -v '^#' .env.ci | xargs --null)
 
 python manage.py collectstatic --noinput
