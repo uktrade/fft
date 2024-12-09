20700234df61d1d42b3c4d22d36600a15edc689f
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-12-09 14:47:57 +0000

    wip

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Exit early if something goes wrong
-set -e
+# Exit early if something goes wrong and 
+set -ex
 
 # Add commands below to run inside the container after all the other buildpacks have been applied
 export $(grep -v '^#' .env | xargs -d '\n')
