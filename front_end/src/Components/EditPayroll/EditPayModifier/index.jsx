bda4dc423accd09a134469e8c5855ccbb3bec4d4
Caitlin Barnard <caitlin@madetech.com>
2024-12-12 20:48:24 +0000

    Fix headers for pay modifier component

--- front_end/src/Components/EditPayroll/EditPayModifier/index.jsx
+++ front_end/src/Components/EditPayroll/EditPayModifier/index.jsx
@@ -1,10 +1,11 @@
-const EditPayModifier = ({ obj }) => {
-  const headers = monthsTitle;
+import { monthsToTitleCase } from "../../../Util";
+
+const EditPayModifier = ({ pay_modifier }) => {
   return (
     <table className="govuk-table">
       <thead className="govuk-table__head">
         <tr className="govuk-table__row">
-          {headers.map((header) => {
+          {monthsToTitleCase.map((header) => {
             return (
               <th scope="col" className="govuk-table__header" key={header}>
                 {header}
