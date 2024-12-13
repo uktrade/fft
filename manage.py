0668cb36602c626f1eba896b57c4b01c1c7ea08d
Suresh Manga <suresh.manga@digital.trade.gov.uk>
2024-12-13 20:40:50 +0000

    Added exception handling

--- manage.py
+++ manage.py
@@ -5,32 +5,29 @@ import sys
 def initialize_debugpy():
     import debugpy
 
-    if not os.getenv("RUN_MAIN"):
+    try:
         debugpy.listen(("0.0.0.0", 5678))
         sys.stdout.write("debugpy listening on port 5678...\n")
+    except Exception as exc:
+        sys.stderr.write(f"Failed to initialize debugpy: {exc}\n")
+
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
+
     try:
         from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django
-        except ImportError as exc:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            ) from exc
-        raise
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
     execute_from_command_line(sys.argv)
+
 
 if __name__ == "__main__":
     ENABLE_DEBUGPY = os.getenv("ENABLE_DEBUGPY")
-    print("DEBUGPY: ", ENABLE_DEBUGPY)
     if ENABLE_DEBUGPY and ENABLE_DEBUGPY.lower() == "true":
         initialize_debugpy()
     main()
