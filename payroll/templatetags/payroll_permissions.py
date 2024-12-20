c4adee401094da0e21726b73cdd57aeb5415b5f5
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2024-12-20 10:36:21 +0000

    begin permissions work

--- payroll/templatetags/payroll_permissions.py
+++ payroll/templatetags/payroll_permissions.py
@@ -1,9 +1,11 @@
 from django import template
 
+import payroll.services.payroll as payroll_service
+
 
 register = template.Library()
 
 
 @register.simple_tag
 def can_edit_payroll(user):
-    return user.is_superuser
+    return payroll_service.can_edit_payroll(user)
