a1d5b82a8cfb087d70b12e571535a6c66fa01389
Denislav Davidov <denislav.davidov@digital.trade.gov.uk>
2025-01-03 12:37:36 +0000

    fixing formatiing errors

--- config/settings/local.py
+++ config/settings/local.py
@@ -14,11 +14,11 @@ ASYNC_FILE_UPLOAD = False
 
 AXES_ENABLED = False
 
-INSTALLED_APPS.append('debug_toolbar')
-MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+INSTALLED_APPS.append("debug_toolbar")
+MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
 DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': lambda request: True,
+    "SHOW_TOOLBAR_CALLBACK": lambda request: True,
 }
--- config/urls.py
+++ config/urls.py
@@ -52,9 +52,10 @@ else:
     admin.site.site_header = "Finance Forecast Tool Admin"
     admin.site.site_title = "Finance Forecast Tool Admin"
     admin.site.index_title = "Welcome to the FFT admin site"
-    
-if hasattr(settings, 'DEBUG_TOOLBAR_CONFIG'):
+
+if hasattr(settings, "DEBUG_TOOLBAR_CONFIG"):
     import debug_toolbar
+
     urlpatterns.append(
-        path('__debug__/', include(debug_toolbar.urls)),
+        path("__debug__/", include(debug_toolbar.urls)),
     )
