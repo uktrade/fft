7a2de6b2e54d40ce51cf72a12c6e4022b3cc37bf
Denislav Davidov <denislav.davidov@digital.trade.gov.uk>
2025-01-03 17:06:59 +0000

    fixing requested changes

--- config/settings/local.py
+++ config/settings/local.py
@@ -16,9 +16,7 @@ AXES_ENABLED = False
 
 INSTALLED_APPS.append("debug_toolbar")
 MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+
 DEBUG_TOOLBAR_CONFIG = {
-    "SHOW_TOOLBAR_CALLBACK": lambda request: True,
+    "SHOW_TOOLBAR_CALLBACK": lambda request: DEBUG,
 }
