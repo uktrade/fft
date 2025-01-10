ee9e3081aacd6595bb462e60b07a61d2982d03c2
Caitlin Barnard <caitlin@madetech.com>
2025-01-10 14:28:18 +0000

    Make component for successBanner

--- front_end/src/Apps/Payroll.jsx
+++ front_end/src/Apps/Payroll.jsx
@@ -12,6 +12,7 @@ import Tabs, { Tab } from "../Components/EditPayroll/Tabs";
 import EditPayModifier from "../Components/EditPayroll/EditPayModifier";
 import ToggleCheckbox from "../Components/Common/ToggleCheckbox";
 import ErrorSummary from "../Components/Common/ErrorSummary";
+import SuccessBanner from "../Components/Common/SuccessBanner";
 
 const initialPayrollState = {
   employees: [],
@@ -124,18 +125,7 @@ export default function Payroll() {
 
   return (
     <>
-      {saveSuccess && (
-        <div className="govuk-notification-banner govuk-notification-banner--success">
-          <div className="govuk-notification-banner__header">
-            <h2
-              className="govuk-notification-banner__title"
-              id="govuk-notification-banner-title"
-            >
-              Success
-            </h2>
-          </div>
-        </div>
-      )}
+      {saveSuccess && <SuccessBanner />}
       {saveError && <ErrorSummary errors={errors} />}
       <ToggleCheckbox
         toggle={hidePreviousMonths}
---
+++ front_end/src/Components/Common/SuccessBanner/index.jsx
@@ -0,0 +1,16 @@
+export default function SuccessBanner() {
+  return (
+    <>
+      <div className="govuk-notification-banner govuk-notification-banner--success">
+        <div className="govuk-notification-banner__header">
+          <h2
+            className="govuk-notification-banner__title"
+            id="govuk-notification-banner-title"
+          >
+            Success
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+}
