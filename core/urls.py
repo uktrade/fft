0152d6a09fc1967328eea179a423043729b48435
Sam Dudley <samuel.dudley@digital.trade.gov.uk>
2025-02-14 15:03:36 +0000

    test streaming response

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,22 @@
+import time
+from django.http import HttpResponse, StreamingHttpResponse
 from django.urls import path
 
 from core import views
+
+
+def slow(request):
+    time.sleep(35)
+    return HttpResponse()
+
+
+def slow_stream(request):
+    def response():
+        for i in range(35):
+            yield i
+            time.sleep(1)
+
+    return StreamingHttpResponse(response())
 
 
 urlpatterns = [
@@ -8,4 +24,6 @@ urlpatterns = [
     path("logout", views.logout, name="logout"),
     path("accessibility", views.AccessibilityPageView.as_view(), name="accessibility"),
     path("report/budget-report", views.budget_report, name="budget_report"),
+    path("slow", slow, name="slow"),
+    path("slow-stream", slow_stream, name="slow-stream"),
 ]
