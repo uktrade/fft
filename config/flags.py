2d861a4df736921133e69e8c6453b72e6077e0c6
Caitlin Barnard <caitlin@madetech.com>
2025-02-25 11:28:45 +0000

    Basic new table with feature flag

--- config/flags.py
+++ config/flags.py
@@ -1,3 +1,4 @@
 ACTUALISATION = "actualisation"
 # TODO (FFT-176): Payroll post-release cleanup
 PAYROLL = "payroll"
+PAYROLL_NEW_TABLE = "payroll_new_table"
---
+++ front_end/src/Components/EditPayroll/PayrollNewTable/index.jsx
@@ -0,0 +1,93 @@
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+} from "@tanstack/react-table";
+import { monthsToTitleCase } from "../../../Util";
+
+function PayrollNewTable({ data, onTogglePayPeriods }) {
+  const monthColumns = monthsToTitleCase.map((header, index) => ({
+    header: header,
+    id: header.toLowerCase(),
+    accessorFn: (row) => row.pay_periods[index],
+    cell: ({ getValue, row }) => (
+      <input
+        type="checkbox"
+        checked={getValue()}
+        onChange={() => onTogglePayPeriods(row.original.id, index, getValue())}
+      />
+    ),
+  }));
+  const employeeColumns = [
+    {
+      accessorKey: "name",
+      header: "Name",
+    },
+    {
+      accessorKey: "grade",
+      header: "Grade",
+    },
+    {
+      accessorKey: "employee_no",
+      header: "Employee No",
+    },
+    {
+      accessorKey: "fte",
+      header: "FTE",
+    },
+    {
+      accessorKey: "programme_code",
+      header: "Programme Code",
+    },
+    {
+      accessorKey: "budget_type",
+      header: "Budget Type",
+    },
+    {
+      accessorKey: "assignment_status",
+      header: "Assignment Status",
+    },
+  ];
+  const columns = [...employeeColumns, ...monthColumns];
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="new-table scrollable">
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th colSpan={header.colSpan} key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default PayrollNewTable;
